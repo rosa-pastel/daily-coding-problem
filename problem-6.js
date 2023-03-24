8455723b2dda4be653e3f9c22353291d98afd46e
rosa-pastel <zeynepranamutlu@outlook.com>
2023-03-25 00:08:51 +0300

    Not solve problem 6

---
+++ problem-6.js
@@ -0,0 +1,6 @@
+/*  
+Problem: Implement an XOR linked list; it has an add(element) which adds
+the element to the end, and a get(index) which returns the node at index.
+
+I'm skipping this because appearently you can't have XOR's in JavaScript
+*/
